446b0788880efcaf2a96153fa5ce8ac510bd61df
kevinjss1 <kevinjss1mc@gmail.com>
2015-03-21 10:48:42 -0400

    Coded Plugin
    Made the plugin

---
+++ src/org/kevinjss1/bukkit/AntiWeather/Main.java
@@ -0,0 +1,31 @@
+package org.kevinjss1.bukkit.AntiWeather;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.weather.WeatherChangeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+	public void onEnable(){
+		saveDefaultConfig();
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+	}
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void rain(WeatherChangeEvent e) {
+	  List<String> worlds = getConfig().getStringList("worlds");
+	  for(String w : worlds) {
+		World world = Bukkit.getWorld(w);
+		if(e.getWorld().equals(world)) {
+	      e.setCancelled(true);
+		  World ww = Bukkit.getServer().getWorld(w);
+		  ww.setStorm(false);
+		}
+	  }
+	}
+
+}
